aa0fc6d3b7109e344482b116eff00479edabfab3
brudnevskij <forbiddenpass@gmail.com>
2022-06-04 02:30:40 +0300

    added function declaration

--- exercises/exercises.hs
+++ exercises/exercises.hs
@@ -194,35 +194,46 @@ ifOp ((L xs):(L ys):(S zs):rls)
                               | zs == "false" = xs
                               | otherwise = []
 
+def ((L body):(S name):rls) = (tail name , body)
+
+
 prtStr [] = return ()
 prtStr (x:xs)
         | null xs = putChar x
         | otherwise = putChar x >> prtStr xs 
 
-polish' (xs, ys)
-               | null xs = prtStr("\x1b[32m"++ (strstack ys')++"\x1b[0m"++(strstack xs') ++"\n")
-               | otherwise = prtStr("\x1b[32m"++ (strstack ys') ++"\x1b[0m"++(strstack xs') ++ "\n") >> polish' (xs', ys')
-               where (xs', ys') = polish xs ys
+fsearch s (x:xs)
+             | fst x == s =(True, snd x)
+             | otherwise = fsearch s xs
+fsearch s [] = (False, [])
 
-polish (L x:xs) ys = (xs, (L x:ys))
-polish (I x:xs) ys =  (xs, (I x:ys))
-polish (S x:xs) ys | x == "-" = (xs, ((I (convertI(head (tail ys)) - convertI(head ys))): tail (tail ys)))
-                   | x == "+" = (xs, ((I (convertI(head (tail ys)) + convertI(head ys))): tail (tail ys)))
-                   | x == "*" = (xs, ((I (convertI(head (tail ys)) * convertI(head ys))): tail (tail ys)))
-                   | x == "dup" = (xs, ((head ys):ys))
-                   | x == "/" = (xs, (I (div (convertI(head(tail ys))) (convertI (head ys))):tail(tail ys)))
-                   | x == "swap" = (xs, ((head (tail ys)):(head ys): (tail(tail ys))))
-                   | x == "drop" = (xs, (tail ys))
-                   | x == "first" = (xs, ((first(head ys)): tail ys))
-                   | x == "cons" = (xs, ( cons (head(tail ys)) (head ys):(tail(tail ys)) ))
-                   | x == "rest" = (xs, (rest (head ys):(tail ys)))
-                   | x == "true" = (xs, (S x):ys)
-                   | x == "false" = (xs, (S x):ys)
-                   | x == "null" = (xs, null'(head ys):tail ys)
-                   | x == "if" = (((ifOp ys)++xs), tail(tail (tail(ys))))
-                   | (head x) == ':' = (xs, (S x:ys))
-                   | otherwise = (xs, (ys))
-polish [] ys = ([], ys)
+polish' (xs, ys, zs)
+               | null xs = prtStr("\x1b[32m"++ (strstack ys')++"\x1b[0m"++(strstack xs') ++"\n")
+               | otherwise = prtStr("\x1b[32m"++ (strstack ys') ++"\x1b[0m"++(strstack xs') ++ "\n") >> polish' (xs', ys', zs')
+               where (xs', ys', zs') = polish xs ys zs
+
+polish (L x:xs) ys zs = (xs, (L x:ys), zs)
+polish (I x:xs) ys zs =  (xs, (I x:ys), zs)
+polish (S x:xs) ys zs
+                   | x == "-" = (xs, ((I (convertI(head (tail ys)) - convertI(head ys))): tail (tail ys)), zs)
+                   | x == "+" = (xs, ((I (convertI(head (tail ys)) + convertI(head ys))): tail (tail ys)), zs)
+                   | x == "*" = (xs, ((I (convertI(head (tail ys)) * convertI(head ys))): tail (tail ys)), zs)
+                   | x == "dup" = (xs, ((head ys):ys), zs)
+                   | x == "/" = (xs, (I (div (convertI(head(tail ys))) (convertI (head ys))):tail(tail ys)), zs)
+                   | x == "swap" = (xs, ((head (tail ys)):(head ys): (tail(tail ys))), zs)
+                   | x == "drop" = (xs, (tail ys), zs)
+                   | x == "first" = (xs, ((first(head ys)): tail ys), zs)
+                   | x == "cons" = (xs, ( cons (head(tail ys)) (head ys):(tail(tail ys)) ), zs)
+                   | x == "rest" = (xs, (rest (head ys):(tail ys)), zs)
+                   | x == "true" = (xs, (S x):ys, zs)
+                   | x == "false" = (xs, (S x):ys, zs)
+                   | x == "null" = (xs, null'(head ys):tail ys, zs)
+                   | x == "if" = (((ifOp ys)++xs), tail(tail (tail(ys))), zs)
+                   | (head x) == ':' = (xs, (S x:ys), zs)
+                   | x == "def" = (xs, tail(tail ys), def(ys):zs)
+                   | fst(fsearch x zs) = (snd(fsearch x zs)++xs, ys, zs)
+                   | otherwise = (xs, (ys), zs)
+polish [] ys zs = ([], ys, zs)
 
 
 
